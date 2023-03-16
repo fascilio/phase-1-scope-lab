5cea2361857222d3164c6487056ce2352cb4d5f8
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-03-16 14:34:26 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName ='BOB';
+   return customerName;
+}
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavouriteCustomer = 'kev';
+function changeLeastFavoriteCustomer(){
+    leastFavouriteCustomer = 'bry'
+}
